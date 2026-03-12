ca3d3f1ed767b3d5f104e2f57001942fa3f3cdfd
imra-hue <imraoficaldev@example.com>
2026-03-12 12:46:22 -0700

    fix railway port

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request , jsonify , render_template
 from calculator import calculate
-
+import os
 app = Flask(__name__)
 
 @app.route("/")
@@ -20,4 +20,5 @@ def calculate_api():
     return  jsonify({"result":result})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
